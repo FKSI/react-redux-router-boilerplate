74a4e303a3160ff6927f22677a1ebb9e8f3bd77c
FKSI <francois.ky@axa-groupsolutions.com>
2017-03-16 20:43:32 +0100

    Fix unies import

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { compose, createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import apiMiddleware from './middleware/api'
